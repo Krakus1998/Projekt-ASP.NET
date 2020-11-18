7ee8682e4b25d6f3c4970cd2428f4ed41283e670
Kamil Rydz <kamil.rydz@microsoft.wsei.edu.pl>
2020-11-18 23:30:54 +0100

    Add project files.

---
+++ Projekt ASP.NET/Models/FakeProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_ASP.NET.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+
+        public IQueryable<Product> Products => new List<Product>
+            {
+                new Product {ID=1, Name = "Ein",Description="Ein",Price=1000000,Category="Banan"},
+                new Product {ID=2, Name = "Zwei",Description="Ein",Price=1000000,Category="Banan"},
+                new Product {ID=3, Name = "Drei",Description="Ein",Price=1000000,Category="Banan"}
+            }.AsQueryable<Product>();
+        }
+}
---
+++ Projekt ASP.NET/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_ASP.NET.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        
+
+    }
+}
